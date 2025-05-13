a2beb80f6b29fe8c78bb22dc809f4a903d01f6e4
Daisy Nardella <daisy50@live.it>
2025-05-13 23:04:55 +0200

    terminato selfwork

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$age = 33;
+$height = 1.60;
+$name = "Daisy";
+$is_student = true;
+
+var_dump($age);
+var_dump($height);
+var_dump($name);
+var_dump($is_student);
+
+const AGE = 33;
+const HEIGHT = 1.60;
+const NAME = "Daisy";
+const IS_STUDENT = true;
+
+var_dump(AGE);
+var_dump(HEIGHT);
+var_dump(NAME);
+var_dump(IS_STUDENT);
