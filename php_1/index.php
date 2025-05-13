b6e500f7832cb42779e6f9d90eacb23bc84a0847
Daisy Nardella <daisy50@live.it>
2025-05-13 23:58:48 +0200

    terminata seconda parte

---
+++ php_1/index.php
@@ -0,0 +1,38 @@
+<?php
+
+// Primo Selfwork
+
+$age = 33;
+$height = 1.60;
+$name = "Daisy";
+$is_student = true;
+
+var_dump($age);
+var_dump($height);
+var_dump($name);
+var_dump($is_student);
+
+const AGE = 33;
+const HEIGHT = 1.60;
+const NAME = "Daisy";
+const IS_STUDENT = true;
+
+var_dump(AGE);
+var_dump(HEIGHT);
+var_dump(NAME);
+var_dump(IS_STUDENT);
+
+// Secondo Selfwork:
+
+$text1 = "Marco";
+$text2 = "hai";
+$text_3 = "sete";
+$text4 = "?";
+$text5 = "Perchè";
+$text6 = '$text2';
+$text7 = 'bevuto';
+$text8 = "tutto";
+
+echo $text1." ".$text2." ".$text_3.$text4." ".$text5." ".$text2." ".$text7." ".$text8.".\n";
+
+?>
