573129789fa04b550e7e734a8abdeae87ac645e3
Daisy Nardella <daisy50@live.it>
2025-05-14 18:45:55 +0200

    terminato terzo selfwork

--- php_1/index.php
+++ php_1/index.php
@@ -1,5 +1,58 @@
 <?php
 
+// Terzo Selfwork
+$words1 = [
+  'vostro',
+  67,
+  'essere',
+  'colle',
+  'mi',
+  'sempre',
+[
+  'oscura',
+  'era',
+  89,
+  [
+  'mezzo',
+   'E'
+  ],
+'ritrovai',
+'per'
+],
+'diritta'
+];
+
+$words2 = [
+  'elemento1' => 25.89,
+  'elemento2' => 'possa',
+  'elemento3' => [
+    'Virgilio',
+    'Favore',
+    'favore',
+    ['fortuna']
+  ],
+ 'fine' => '!'
+];
+
+$results = "Nel " .$words1[6][3][0]. " di cammin di nostra vita ".$words1[4]." ".$words1[6][4]." ".$words1[6][5]." una selva ".$words1[6][0].", che' la ".$words1[7]." via era smarrita." ."\n";
+echo $results;
+
+
+
+// Secondo Selfwork:
+
+$text1 = "Marco";
+$text2 = "hai";
+$text_3 = "sete";
+$text4 = "?";
+$text5 = "Perchè";
+$text6 = '$text2';
+$text7 = 'bevuto';
+$text8 = "tutto";
+
+echo $text1." ".$text2." ".$text_3.$text4." ".$text5." ".$text2." ".$text7." ".$text8.".\n";
+
+
 // Primo Selfwork
 
 $age = 33;
@@ -22,17 +75,6 @@ var_dump(HEIGHT);
 var_dump(NAME);
 var_dump(IS_STUDENT);
 
-// Secondo Selfwork:
 
-$text1 = "Marco";
-$text2 = "hai";
-$text_3 = "sete";
-$text4 = "?";
-$text5 = "Perchè";
-$text6 = '$text2';
-$text7 = 'bevuto';
-$text8 = "tutto";
-
-echo $text1." ".$text2." ".$text_3.$text4." ".$text5." ".$text2." ".$text7." ".$text8.".\n";
 
 ?>
